2df6d3fc9469f0cc9f584db174a9089c1ef6052b
albigrainca <albi.grainca@uha.fr>
2024-01-28 11:45:23 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Atalanta GRAINCA on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
